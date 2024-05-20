3138b1c3a4fa520b2c43d6a05cba4d166e382ac1
Pedro Lucas <pedro.lucas0801@outlook.com>
2024-05-19 23:15:32 -0300

    Alteracao no import para Windows

--- Scripts/MalhaAberta/sim_degrau_malha_aberta.py
+++ Scripts/MalhaAberta/sim_degrau_malha_aberta.py
@@ -1,3 +1,14 @@
+import sys
+import os
+
+# Adiciona o diretório 'Controle' ao sys.path
+current_dir = os.path.dirname(__file__)
+parent_dir = os.path.abspath(os.path.join(current_dir, "..", ".."))
+controle_dir = os.path.abspath(os.path.join(parent_dir, "Controle"))
+print(f"Adicionando {controle_dir} ao sys.path")
+if controle_dir not in sys.path:
+    sys.path.append(controle_dir)
+
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from Controle import *
@@ -10,13 +21,12 @@ def sim_degrau_malha_aberta(sys):
     T = 20
 
     input = 0
-    ref = 1 *np.pi/180
+    ref = 1 * np.pi / 180
 
-    show_var = "estados" # "saidas" / "estados" / "controles"
-    estados = []  # indice do estado que você que observar (referência vem da matriz C, neste caso 0 corresponde à vel. horizontal)
-    
-
-
+    show_var = "estados"  # "saidas" / "estados" / "controles"
+    estados = (
+        []
+    )  # indice do estado que você que observar (referência vem da matriz C, neste caso 0 corresponde à vel. horizontal)
 
     # === Sistema === #
     B2 = sys.B
@@ -28,52 +38,54 @@ def sim_degrau_malha_aberta(sys):
     nu = B2.shape[1]
 
     # === Contas === #
-    yout, t, xout = cmat.step(sys,T, input=input, return_x=True)
+    yout, t, xout = cmat.step(sys, T, input=input, return_x=True)
 
-    if show_var=="saidas": # mostrar saídas, matriz C
-        if len(estados)==0:
+    if show_var == "saidas":  # mostrar saídas, matriz C
+        if len(estados) == 0:
             estados = range(yout.shape[1])
-        x = ref * yout[:,estados,0]
+        x = ref * yout[:, estados, 0]
         labels = lbl_saidas
-    elif show_var=="estados": # mostrar estados, matriz A
-        if len(estados)==0:
+    elif show_var == "estados":  # mostrar estados, matriz A
+        if len(estados) == 0:
             estados = range(xout.shape[1])
-        x = ref * xout[:,estados,0]
+        x = ref * xout[:, estados, 0]
         labels = lbl_estados
-    elif show_var=="controles":
-        if len(estados)==0:
+    elif show_var == "controles":
+        if len(estados) == 0:
             estados = range(nu)
-        x = np.zeros((len(t),nu))
-        x[:,input] += ref
+        x = np.zeros((len(t), nu))
+        x[:, input] += ref
         labels = lbl_controle
 
     # === Plot === #
     fig = go.Figure()
 
     for i in range(x.shape[1]):
-        fig.add_trace(go.Scatter(
-            x=t, y=x[:,i],
-            mode = "lines",
-            name = labels[i],
-        ))
-    
+        fig.add_trace(
+            go.Scatter(
+                x=t,
+                y=x[:, i],
+                mode="lines",
+                name=labels[i],
+            )
+        )
+
     fig.update_layout(
         title="Resposta a entrada degrau",
-        xaxis = dict(
-            title= "Tempo [s]",
+        xaxis=dict(
+            title="Tempo [s]",
             # range = [min(self.WSs), max(self.WSs)],
             # nticks = 50,
             # showgrid = True,
             # gridcolor = "lightgrey",
         ),
-        yaxis = dict(
+        yaxis=dict(
             title="Variáveis de estado",
             # range = [min(self.PWs), max(self.PWs)],
             # dtick = 0,
             # tickrange = [0,1],
             # gridcolor = "lightgrey"
         ),
-
         showlegend=True,
         legend=dict(
             orientation="h",
@@ -83,27 +95,24 @@ def sim_degrau_malha_aberta(sys):
                 size=10,
                 # color="black"
             ),
-
             yanchor="top",
-            y=-.20,
+            y=-0.20,
             xanchor="center",
             x=0.5,
-
             # bordercolor="Black",
             # borderwidth=1,
         ),
-
         plot_bgcolor="White",
         paper_bgcolor="White",
-        template = "plotly_white",
-        width=700, height=500,
+        template="plotly_white",
+        width=700,
+        height=500,
     )
 
     return fig
 
 
-
-if __name__=="__main__":
-    fig = sim_degrau_malha_aberta(sist.sys_malha_aberta) # type: ignore
+if __name__ == "__main__":
+    fig = sim_degrau_malha_aberta(sist.sys_malha_aberta)  # type: ignore
     fig.show()
     fig.write_image("sim_degrau_malha_aberta.png", scale=2)
--- Scripts/Moderno/sim_inicial_malha_fechada.py
+++ Scripts/Moderno/sim_inicial_malha_fechada.py
@@ -1,34 +1,43 @@
+import sys
+import os
+
+# Adiciona o diretório 'Controle' ao sys.path
+diretorio_atual = os.path.dirname(__file__)
+diretorio_pai = os.path.abspath(os.path.join(diretorio_atual, "..", "..", ".."))
+diretorio_controle = os.path.abspath(os.path.join(diretorio_pai, "Controle"))
+diretorio_scripts = os.path.abspath(os.path.join(diretorio_pai, "Scripts"))
+
+if diretorio_controle not in sys.path:
+    sys.path.append(diretorio_controle)
+
+if diretorio_scripts not in sys.path:
+    sys.path.append(diretorio_scripts)
+
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from Controle import *
 from Controle.Moderno import *
 
 
-def sim_inicial_malha_fechada(sistema,K):
+def sim_inicial_malha_fechada(sistema, K):
 
     # === Configuração === #
-    T = 20
+    T = 10
 
-    X0 = [
-        0,
-        0,
-        0,
-        10/57.3
-    ]
+    X0 = [0, 0, 0, 10 / 57.3]
 
-    show_var = "estados" # "saidas" / "estados" / "controles"
-    estados = []  # indice do estado que você que observar (referência vem da matriz C, neste caso 0 corresponde à vel. horizontal)
-    
-
-
+    show_var = "estados"  # "saidas" / "estados" / "controles"
+    estados = (
+        []
+    )  # indice do estado que você que observar (referência vem da matriz C, neste caso 0 corresponde à vel. horizontal)
 
     # === Sistema === #
     # matrizes do sistema
-    A  = sistema.A
+    A = sistema.A
     B2 = sistema.B2
     B1 = sistema.B1
-    C  = sistema.C
-    D  = sistema.D
+    C = sistema.C
+    D = sistema.D
 
     lbl_estados = sistema.estados
     lbl_disturbios = sistema.perturbacoes
@@ -41,57 +50,59 @@ def sim_inicial_malha_fechada(sistema,K):
     nu = B2.shape[1]
 
     # === Contas === #
-    F = A - B2@K
-    Cr = C - D@K
+    F = A - B2 @ K
+    Cr = C - D @ K
     B0 = np.zeros(B2.shape)
     D0 = np.zeros(D.shape)
     sys = ct.ss(F, B0, Cr, D0)
     print(ct.damp(sys))
-    yout, t, xout = cmat.initial(sys,T, X0=X0, return_x=True)
+    yout, t, xout = cmat.initial(sys, T, X0=X0, return_x=True)
 
-    if show_var=="saidas": # mostrar saídas, matriz C
-        if len(estados)==0:
+    if show_var == "saidas":  # mostrar saídas, matriz C
+        if len(estados) == 0:
             estados = range(yout.shape[1])
-        x = yout[:,estados]
+        x = yout[:, estados]
         labels = lbl_saidas
-    elif show_var=="estados": # mostrar estados, matriz A
-        if len(estados)==0:
+    elif show_var == "estados":  # mostrar estados, matriz A
+        if len(estados) == 0:
             estados = range(xout.shape[1])
-        x = xout[:,estados]
+        x = xout[:, estados]
         labels = lbl_estados
-    elif show_var=="controles":
-        if len(estados)==0:
+    elif show_var == "controles":
+        if len(estados) == 0:
             estados = range(nu)
-        x = - np.array(xout[:,:] @ K.transpose())
+        x = -np.array(xout[:, :] @ K.transpose())
         labels = lbl_controle
 
     # === Plot === #
     fig = go.Figure()
 
     for i in range(x.shape[1]):
-        fig.add_trace(go.Scatter(
-            x=t, y=x[:,i],
-            mode = "lines",
-            name = labels[i],
-        ))
-    
+        fig.add_trace(
+            go.Scatter(
+                x=t,
+                y=x[:, i],
+                mode="lines",
+                name=labels[i],
+            )
+        )
+
     fig.update_layout(
         title="Resposta a condição inicial",
-        xaxis = dict(
-            title= "Tempo [s]",
+        xaxis=dict(
+            title="Tempo [s]",
             # range = [min(self.WSs), max(self.WSs)],
             # nticks = 50,
             # showgrid = True,
             # gridcolor = "lightgrey",
         ),
-        yaxis = dict(
-            title="Variáveis de estado",
+        yaxis=dict(
+            title="Variáveis de " + show_var,
             # range = [min(self.PWs), max(self.PWs)],
             # dtick = 0,
             # tickrange = [0,1],
             # gridcolor = "lightgrey"
         ),
-
         showlegend=True,
         legend=dict(
             orientation="h",
@@ -101,36 +112,44 @@ def sim_inicial_malha_fechada(sistema,K):
                 size=10,
                 # color="black"
             ),
-
             yanchor="top",
-            y=-.20,
+            y=-0.20,
             xanchor="center",
             x=0.5,
-
             # bordercolor="Black",
             # borderwidth=1,
         ),
-
         plot_bgcolor="White",
         paper_bgcolor="White",
-        template = "plotly_white",
-        width=700, height=500,
+        template="plotly_white",
+        width=700,
+        height=500,
     )
 
     return fig
 
 
-
-if __name__=="__main__":
-    # K = sist.Controlador.LQR.P.Ganhos # type: ignore
+if __name__ == "__main__":
+    # K = sist.Controlador.LQR.P.Ganhos  # type: ignore
     # K = sist.Controlador.Alocacao.P3.Ganhos # type: ignore
 
     # polos = np.array([-5 +10j , -5 -10j ,-0.27+0.83j, -0.27-0.83j])
     # polos = np.array([-0.20 + .20j, -0.20 - 0.20j, -4.5 + 9j, -4.5 - 9j])
     # polos = np.array([-5 + 10j, -5 - 10j, -.81 + .81j, -.81 - .81j])
-    polos = np.array([-4 + 5j, -4 - 5j, -.5 + .4j, -.5 - .4j])
-    controlador = controlador_alocacao_polos(sist.sistema.A, sist.sistema.B1, sist.sistema.B2, sist.sistema.C, sist.sistema.D, polos)
-    K = controlador.Ganhos
-    fig = sim_inicial_malha_fechada(sist.sistema,K) # type: ignore
+    # polos = np.array([-4 + 5j, -4 - 5j, -0.5 + 0.4j, -0.5 - 0.4j])
+    from main import sist
+
+    K = sist.Controlador.LQR.P.Ganhos
+
+    # controlador = controlador_alocacao_polos(
+    #     sist.sistema.A,
+    #     sist.sistema.B1,
+    #     sist.sistema.B2,
+    #     sist.sistema.C,
+    #     sist.sistema.D,
+    #     polos,
+    # )
+    # K = controlador.Ganhos
+    fig = sim_inicial_malha_fechada(sist.sistema, K)  # type: ignore
     fig.show()
-    fig.write_image("sim_inicial_malha_fechada.png", scale=2)
+    fig.write_image("cenario_estados_LQR.png", scale=2)
